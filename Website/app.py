caebe076bbe99ed7559ea5bc60a567dac0b17240
MaxH403 <mxherms403@gmail.com>
2023-08-02 16:17:47 -0700

    good

--- Website/app.py
+++ Website/app.py
@@ -6,10 +6,10 @@ import os
 app = FastAPI()
 
 
-
 # mount static files directories (only for static file like css)
 app.mount("/css", StaticFiles(directory="css"), name="css")
-
+app.mount("/js", StaticFiles(directory="js"), name="js")
+app.mount("/images", StaticFiles(directory="images"), name="images")
 
 # this function does nothing yet
 @app.get("/data")
@@ -17,12 +17,17 @@ async def get_data():
     # replace with actual data
     return {"message": "Test"}
 
+# sends GET request when entering webpage and displays main webpage
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
     # reads Wordle.html file and returns html file with status code 200
-    with open(os.path.join('html', 'Wordle.html'), 'r') as f:
-        html_content = f.read()
+    with open(os.path.join('html', 'Wordle.html'), 'r') as file:
+        html_content = file.read()
     return HTMLResponse(content=html_content, status_code=200)
 
-
-
+# sends GET request when clicking Game Description and opens new webpage
+@app.get("/game_description", response_class=HTMLResponse)
+async def read_description():
+    with open(os.path.join("html", "Game_description.html"), "r") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
